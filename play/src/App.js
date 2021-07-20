c17813087db8370d1d4a4436ea0f5a29e08b2bc2
vipulchakravarthy <saivipul15@msitprogram.net>
2021-07-20 09:45:31 +0530

    removed unwanted code

--- play/src/App.js
+++ play/src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-import ListGroup from 'react-bootstrap/ListGroup';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const variant = ["success", "danger", "warning"]
   return (
-    <div className="App">
+    <div className="">
       <h3>Vipul</h3>
-      <ListGroup>
-        <ListGroup.Item>No style</ListGroup.Item>
-        {variant.map((item, index) => {
-          return (
-            <ListGroup.Item key={index} variant={item} onClick={() => alert('card clicked')}>{item}</ListGroup.Item>
-          )
-        })}
-      </ListGroup>
     </div>
   );
 }
